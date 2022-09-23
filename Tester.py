bda7e23d847d5e14232203f0f5daf7092d26633e
Alex Katopodis <aak61@duke.edu>
2022-09-22 21:26:17 -0400

    Removed test file

--- Tester.py
+++
@@ -1,6 +0,0 @@
-from EuropeanOption import *
-
-# (type, underlying, strike, days to exp, rate, div, vol (optional), price (optional), debug)
-option1 = Option("call", 100, 100, 30, 2, 0, 0, 4.65)
-
-print(option1.vol)
